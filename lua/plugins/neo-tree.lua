0c8b5a94a02ea6d2e9d30ab2d378ded781123bb0
hmadej <hubertmadej02@gmail.com>
2024-12-25 18:15:47 +0100

    Initial NVIM config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", 
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem show left<CR>', {})
+    vim.api.nvim_set_keymap('n', '<leader>w', '<C-w>w', { noremap = true, silent = true })
+  end
+}
