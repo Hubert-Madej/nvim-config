4267f61a2324e9313915db39eaa6d93a0a6bfa29
Hubert Madej <hubertmadej02@gmail.com>
2025-01-18 14:24:45 +0100

    added yanking highlighting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
-vim.g.mapleader = " "
 vim.cmd("set t_Co=16")
+vim.g.mapleader = " "
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("vim_highlight_yank", { clear = true }),
+	callback = function()
+		(vim.hl or vim.highlight).on_yank()
+	end,
+})
