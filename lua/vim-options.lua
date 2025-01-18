ba25b85cfeb74c161a785722f5256bfb5fa02925
Hubert Madej <hubertmadej02@gmail.com>
2025-01-18 14:29:18 +0100

    added copy-diagnostics command

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,8 +6,27 @@ vim.cmd("set relativenumber")
 vim.cmd("set t_Co=16")
 vim.g.mapleader = " "
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("vim_highlight_yank", { clear = true }),
-	callback = function()
-		(vim.hl or vim.highlight).on_yank()
-	end,
+  group = vim.api.nvim_create_augroup("vim_highlight_yank", { clear = true }),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
 })
+
+vim.keymap.set("n", "<leader>cd", function()
+  -- Get diagnostics for the current line
+  local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
+  if #diagnostics > 0 then
+    -- Concatenate all diagnostic messages on the line
+    local messages = {}
+    for _, diagnostic in ipairs(diagnostics) do
+      table.insert(messages, diagnostic.message)
+    end
+    local combined_message = table.concat(messages, "\n")
+
+    -- Copy to system clipboard
+    vim.fn.setreg("+", combined_message)
+    print("Diagnostic message copied to clipboard!")
+  else
+    print("No diagnostic message on this line.")
+  end
+end, { noremap = true, silent = true })
